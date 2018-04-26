dc4dc0c155e185e6b3727966c6dd639513a2176f
Jeffrey-Young <youngjt@live.unc.edu>
2018-04-25 21:27:58 -0400

    bump

---
+++ src/assign4/ACustomDuplexObjectClientInputPort.java
@@ -0,0 +1,36 @@
+package assign4;
+
+import java.nio.ByteBuffer;
+
+import inputport.datacomm.duplex.DuplexClientInputPort;
+import inputport.datacomm.duplex.object.ADuplexObjectClientInputPort;
+import inputport.datacomm.duplex.object.DuplexObjectInputPortSelector;
+import inputport.datacomm.duplex.object.explicitreceive.ReceiveReturnMessage;
+/**
+ * Overrides interesting methods of the default object client input port
+ */
+public class ACustomDuplexObjectClientInputPort extends ADuplexObjectClientInputPort {
+
+	public ACustomDuplexObjectClientInputPort(
+			DuplexClientInputPort<ByteBuffer> aBBClientInputPort) {
+		super(aBBClientInputPort);
+	}
+	/**
+	 * Simply traces the sends dispatched to the sueprclass, does nothing more
+	 */
+	@Override
+	public void send(String aDestination, Object aMessage) {
+		System.out.println (aDestination + "<-" + aMessage);
+		super.send(aDestination, aMessage);	
+	}
+	/**
+	 * Overrides the unimplemented paramaterized receive method
+	 */
+	@Override
+	public ReceiveReturnMessage<Object> receive(String aSource) {
+		System.err.println("Receive started");
+		ReceiveReturnMessage retVal = super.receive(aSource);
+		System.out.println (aSource + "<-" + retVal);
+		return retVal;
+	}
+}
---
+++ src/assign4/ACustomDuplexObjectServerInputPort.java
@@ -0,0 +1,36 @@
+package assign4;
+
+import java.nio.ByteBuffer;
+
+import inputport.datacomm.AReceiveRegistrarAndNotifier;
+import inputport.datacomm.ReceiveRegistrarAndNotifier;
+import inputport.datacomm.duplex.DuplexServerInputPort;
+import inputport.datacomm.duplex.object.ADuplexObjectServerInputPort;
+import inputport.datacomm.duplex.object.explicitreceive.ReceiveReturnMessage;
+
+public class ACustomDuplexObjectServerInputPort extends ADuplexObjectServerInputPort{
+
+	public ACustomDuplexObjectServerInputPort(
+			DuplexServerInputPort<ByteBuffer> aBBDuplexServerInputPort) {
+		super(aBBDuplexServerInputPort);
+	}
+	/**
+	 * Changes the notifier that invokes receive listeners
+	 */
+	@Override
+	protected ReceiveRegistrarAndNotifier<Object> createReceiveRegistrarAndNotifier() {
+		return new ACustomReceiveNotifier();
+	}
+	
+	/**
+	 * Overrides the unimplemented paramaterized receive method
+	 */
+	@Override
+	public ReceiveReturnMessage<Object> receive(String aSource) {
+		System.err.println("Receive started");
+		ReceiveReturnMessage retVal = super.receive(aSource);
+		System.out.println (aSource + "<-" + retVal);
+		return retVal;
+	}
+
+}
